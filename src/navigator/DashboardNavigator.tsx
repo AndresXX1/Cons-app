be4132ef3783a79e465b2b3f5c7bb2caeeed9e77
douglas_rondon <douglasgrl27@gmail.com>
2024-06-29 12:44:45 -0400

    shop

--- src/navigator/DashboardNavigator.tsx
+++ src/navigator/DashboardNavigator.tsx
@@ -23,7 +23,7 @@ const DashboardNavigator = () => {
       <NavigationContainer>
         <StatusBar style="light" backgroundColor="#00A5E7" />
         <Tab.Navigator
-          initialRouteName="Home"
+          initialRouteName="Shop"
           tabBar={props => {
             return (
               <View style={styles.navBar}>
