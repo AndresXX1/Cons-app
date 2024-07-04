cc63a212b3bd3657f730dffbdc0721e9e5d922b2
Jioyi <karlosagreda@hotmail.com>
2024-07-03 23:02:23 -0300

    asdasd

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,35 +1,22 @@
-import { useEffect } from "react";
-import { verifySessionAsync, checkOnboarding } from "../store/actions/auth";
-import { AppDispatch, RootState } from "../store";
-import { useDispatch, useSelector } from "react-redux";
-import LoadingScreen from "../pages/LoadingScreen";
-import OnBoardingNavigator from "./OnBoardingNavigator";
-import DashboardNavigator from "./DashboardNavigator";
-import AuthNavigator from "./AuthNavigator";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
+//import { useEffect } from 'react';
+//import { verifySessionAsync, checkOnboarding } from '../store/actions/auth';
+import { AppDispatch, RootState } from '../store';
+import { useDispatch, useSelector } from 'react-redux';
+//import LoadingScreen from '../pages/LoadingScreen';
+import OnBoardingNavigator from './OnBoardingNavigator';
+//import DashboardNavigator from './DashboardNavigator';
+//import AuthNavigator from './AuthNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 
 const MainNavigator = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { isAuth, isLoading, isOnboarding, user } = useSelector(
-    (state: RootState) => state.auth
-  );
-  useEffect(() => {
-    const validateSessions = async () => {
-      await dispatch(checkOnboarding());
-      await dispatch(verifySessionAsync({ dispatch }));
-    };
-    validateSessions();
-  }, [dispatch]);
 
   return (
     <>
       <NavigationContainer>
-      <StatusBar style="light" backgroundColor="#40066C" />
-        {!isOnboarding && (
-          <OnBoardingNavigator />
-        )}
- 
+        <StatusBar style="light" backgroundColor="#40066C" />
+        <OnBoardingNavigator />
       </NavigationContainer>
     </>
   );
